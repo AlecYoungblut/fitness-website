50136293f8957717c109d0a805868dba895feecd
Alec Youngblut <me@alecyoungblut.ca>
2020-10-29 14:46:04 -0400

    Added FontAwesome

---
+++ portfolio/src/fontawesome.js
@@ -0,0 +1,14 @@
+// import the library
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// import your icons
+import { faTwitter, faInstagram, faFacebook, faGooglePlus, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+library.add(
+    faTwitter,
+    faInstagram,
+    faFacebook,
+    faGooglePlus,
+    faLinkedin
+    // more icons go here
+);
