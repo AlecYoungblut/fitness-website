294b9c67cc4952d4a6ecf9515bfe94b9db45f383
Alec Youngblut <me@alecyoungblut.ca>
2020-10-29 14:45:27 -0400

    Changed HeroSection, Nav

--- portfolio/src/components/HeroSection.js
+++ portfolio/src/components/HeroSection.js
@@ -3,19 +3,21 @@ import React from "react";
 class HeroSection extends React.Component {
     render() {
         return (
-            <div className="container mx-auto py-20 px-12 bg-gradient-to-r from-purple-500 to-purple-900">
-                <div className="container mx-auto px-6">
-                    <h2 className="text-4xl font-bold mb-2 text-white">
-                        Smart Health Monitoring Wristwatch!
+            <section className="bg-gradient-to-r from-purple-500 to-purple-900">
+                <div className="container mx-auto py-20 px-12 ">
+                    <div className="container mx-auto px-6">
+                        <h2 className="text-4xl font-bold mb-2 text-white">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing.
                     </h2>
-                    <h3 className="text-2xl mb-8 text-gray-200">
-                        Monitor your health vitals smartly anywhere you go.
+                        <h3 className="text-2xl mb-8 text-gray-200">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing.
                     </h3>
-                    <button className="bg-white font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider">
-                        Pre Order
+                        <button className="bg-white font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider">
+                            Lorem
                     </button>
+                    </div>
                 </div>
-            </div>
+            </section>
         );
     }
 };
